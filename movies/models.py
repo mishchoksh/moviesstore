50f17da8f8203f162fc69fe582222cafa4c1aeae
mishchoksh <misha.chokshi@gmail.com>
2025-09-18 15:48:20 -0400

    reply to comment update

--- movies/models.py
+++ movies/models.py
@@ -21,12 +21,12 @@ class Review(models.Model):
     def __str__(self):
         return str(self.id) + ' - ' + self.movie.name
 
-class MovieRequest(models.Model):
+class Reply(models.Model):
     id = models.AutoField(primary_key=True)
+    comment = models.CharField(max_length=255)
+    date = models.DateTimeField(auto_now_add=True)
+    review = models.ForeignKey(Review, on_delete=models.CASCADE, related_name='replies')
     user = models.ForeignKey(User, on_delete=models.CASCADE)
-    name = models.CharField(max_length=255)
-    description = models.TextField()
-    created_at = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
-        return f"{self.user.username} - {self.name}"
+        return f"Reply {self.id} to Review {self.review.id}"
